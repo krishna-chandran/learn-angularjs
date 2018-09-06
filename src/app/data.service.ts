7d17ab4af4d3fb9127d075e5403191d9bb905cdf
Krishnakumar Chandran <krishna-chandran@users.noreply.github.com>
2018-09-06 15:11:14 -0400

    Slash added for individual user details

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -13,7 +13,7 @@ export class DataService {
   }
 
   getUser(userId) {
-    return this.http.get('https://jsonplaceholder.typicode.com/users' + userId)
+    return this.http.get('https://jsonplaceholder.typicode.com/users/' + userId)
   }
 
   getPosts() {
